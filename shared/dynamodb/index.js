479c9f592a892d47761cd27e7a5bc9267e8e583d
SérgioAndrade <sergiovenicio2015@gmail.com>
2021-10-30 10:14:41 -0300

    CRUD operations

--- shared/dynamodb/index.js
+++ shared/dynamodb/index.js
@@ -1,16 +1,67 @@
 const AWS = require('aws-sdk');
 
 class DynamoDB {
-  constructor() {
+  constructor(table) {
+    this._table = table;
     this._client = new AWS.DynamoDB.DocumentClient();
   }
 
-  async putItem(table, item) {
+  async putItem(item) {
     const params = {
-      TableName: table,
+      TableName: this._table,
       Item: item
     };
     return this._client.put(params).promise();
+  }
+
+  async scanItens() {
+    const reponse = await this._client.scan({
+      TableName: this._table
+    }).promise();
+    return reponse.Items.map(item => ({
+      ...item
+    }));
+  }
+
+  async getItem(key) {
+    const reponse = await this._client.get({
+      TableName: this._table,
+      Key: key
+    }).promise();
+
+    return {
+      ...reponse.Item
+    };
+  }
+
+  async deleteItem(key) {
+    return this._client.delete({
+      TableName: this._table,
+      Key: key
+    }).promise();
+  }
+
+  async updateItem(key, newItem) {
+    let expressionValues = [];
+    const ExpressionAttributeNames = {};
+    const ExpressionAttributeValues = {};
+
+    for (const [key, value] of Object.entries(newItem)) {
+      const keyName = `#${key}`;
+      const keyAttr = `:${key}`;
+      expressionValues.push(`${keyName} = ${keyAttr}`);
+      ExpressionAttributeNames[keyName] = key;
+      ExpressionAttributeValues[keyAttr] = value;
+    }
+
+    return await this._client.update({
+      TableName: this._table,
+      Key: key,
+      UpdateExpression: `set ${expressionValues.join(',')}`,
+      ExpressionAttributeNames,
+      ExpressionAttributeValues,
+      ReturnValues: 'ALL_NEW',
+    }).promise();
   }
 }
 
