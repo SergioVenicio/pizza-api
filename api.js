f92c31399b848df8d8cb1798b6867d878a6bc3fc
SérgioAndrade <sergiovenicio2015@gmail.com>
2021-10-30 10:15:00 -0300

    New endpoints

--- api.js
+++ api.js
@@ -1,22 +1,50 @@
 const Api = require('claudia-api-builder');
 const api = new Api();
 
+const OrderHandler = require('./handlers/orderHandler');
 const getPizzas = require('./handlers/get-pizzas');
-const createOrder = require('./handlers/create-order');
+
+const orderHandler = new OrderHandler();
 
 api.get('/', () => 'Welcome to pizza api');
 
 api.get('/pizzas', () => getPizzas());
 
-api.get('/pizzas/{id}', request => {
-  return getPizzas(request.pathParams.id);
-}, {
+api.get('/pizzas/{id}', request => (
+  getPizzas(request)
+), {
   error: 404
 });
 
-api.post('/orders', async request => {
-  return await createOrder(request.body);
-}, {
+api.get('/orders', async () => (
+  await orderHandler.get()
+), {
+  error: 404
+});
+
+api.get('/orders/{id}', async (request) => (
+  await orderHandler.get(request)
+), {
+  error: 404
+});
+
+api.delete('/orders/{id}', async (request) => (
+  await orderHandler.delete(request)
+), {
+  success: 204,
+  error: 404,
+});
+
+api.put('/orders/{id}', async (request) => (
+  await orderHandler.update(request)
+), {
+  success: 201,
+  error: 404
+});
+
+api.post('/orders', async request => (
+  await orderHandler.create(request)
+), {
   success: 201,
   error: 400
 });
